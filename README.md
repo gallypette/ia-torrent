cedb14e1e178c1330353888eef9ba171f084f525
Jean-Louis Huynen <huynenjl@gmail.com>
2021-09-24 11:27:55 +0200

    add [implem] initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# ia-torrent
-Retrive torrent files from an Internet Archive search
+# ia_torrent
+
+ia_torrent retrieves torrent files corresponding to a search request on the Internet Archive.
+
+## Basic setup
+
+Install the requirements:
+```
+$ pip install -r requirements.txt
+```
+
+# Run the application:
+```
+$ python -m ia_torrent --help
+$ python -m ia_torrent <search request>
+$ python -m ia_torrent <search request> --folder [folder]
+```
+For instance:
+```
+python ia_torrent.py 'collection:cdromsoftware AND subject:Microsoft'
+```
---
+++ ia_torrent.py
@@ -0,0 +1,64 @@
+import click
+import requests
+from requests.exceptions import HTTPError
+import urllib.parse
+from pathlib import Path
+import os
+import csv
+
+@click.command()
+@click.argument('query')
+@click.option('--folder', default="torrents", help='Folder in which to save torrent files.')
+def search(query, folder):
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    archive = "https://archive.org/advancedsearch.php?q={}&fl%5B%5D=identifier&rows=1000000000&page=1&output=csv"
+    safe_string = urllib.parse.quote_plus(query)
+    try:
+        r = requests.get(archive.format(query))
+        r.raise_for_status()
+
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+    else:
+        decoded_content = r.content.decode('utf-8')
+        cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+        identifiers = list(cr)
+        for row in identifiers:
+            if row[0] != 'identifier':
+                get_torrent(row[0], folder)
+
+def get_torrent(id, folder):
+    metadata_url = "https://archive.org/metadata/{}"
+    try:
+        req = requests.get(metadata_url.format(id))
+        req.raise_for_status()
+
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+    else:
+        jresponse = req.json()
+        dir = jresponse["dir"]
+        ws = jresponse["workable_servers"][0]
+
+        url = "http://{}{}/{}_archive.torrent"
+        filename = "{}.torrent"
+
+        with open(os.path.join(folder, filename.format(id)), 'wb+') as f:
+
+            try:
+                dl = requests.get(url.format(ws, dir, id))
+                req.raise_for_status()
+            except HTTPError as http_err:
+                print(f'HTTP error occurred: {http_err}')
+            except Exception as err:
+                print(f'Other error occurred: {err}')
+            else:
+                f.write(dl.content)
+
+
+if __name__ == '__main__':
+    search()
